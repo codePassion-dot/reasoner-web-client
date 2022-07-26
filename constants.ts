720ffe71088ec4cc723a7b3de63650d206167c42
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2022-07-25 21:05:00 -0500

    feat(auth): add tab component
    add functionality to render a specific content that depends on the selected tab
    
    Co-authored-by: hrivera7777 <hrivera@unal.edu.co>

---
+++ constants.ts
@@ -0,0 +1,3 @@
+export const CONSTANTS = {
+  tabsTexts: ["Sign in", "Sign up"],
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        cerulean: "#0A6AA6",
+        seagull: "#7EB6D9",
+        "cloud-burst": "#354050",
+      },
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
