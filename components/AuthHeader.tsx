b1806e5691db58e3ad10f5ca00d0e981cac16211
Juan Jacobo Jaramillo <63831898+codePassion-dot@users.noreply.github.com>
2022-07-29 20:51:37 -0500

    feature/auth page (#1)
    * feat(auth): add tab component
    
    add functionality to render a specific content that depends on the selected tab
    
    Co-authored-by: hrivera7777 <hrivera@unal.edu.co>
    
    * feat(auth): add auth header component
    
    we would show a header with a descriptive description depending on the selected tab
    
    Co-authored-by: hrivera7777 <hrivera@unal.edu.co>
    
    * feat(app,auth): add default background
    
    add auth component shadow to make the contrast with the default background
    
    Co-authored-by: hrivera7777 <hrivera@unal.edu.co>
    
    * feat(auth): add sign up form
    
    - add yup for the schema validation
    - add custom input
    - add sign up button
    
    Co-authored-by: hrivera7777 <hrivera@unal.edu.co>
    
    * feat(auth): add generic form component to reuse it for the auth module
    
    Co-authored-by: hrivera7777 <hrivera@unal.edu.co>
    
    Co-authored-by: hrivera7777 <hrivera@unal.edu.co>

---
+++ components/AuthHeader.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import { FC } from "react";
+import leftPerson from "../public/auth-header-left-person.png";
+import rightPerson from "../public/auth-header-right-person.png";
+import {
+  AuthHeaderHorizontalDots,
+  AuthHeaderVerticalDots,
+} from "../public/icons";
+interface Props {
+  children: React.ReactNode;
+  title: string;
+  description: string;
+}
+const AuthHeader: FC<Props> = ({ children, title, description }) => {
+  return (
+    <>
+      <div className="flex relative flex-row justify-between pt-9 bg-gradient-to-r from-denim to-cerulean">
+        <Image src={leftPerson} width={142} height={167} alt="leftPerson" />
+        <div className="flex flex-col basis-8/12">
+          <h1 className="flex flex-col text-5xl font-bold text-center text-white">
+            <span>{title}</span>
+            <span className="mt-2 text-sm text-center">{description}</span>
+            <AuthHeaderHorizontalDots styles="mx-auto mt-3" />
+          </h1>
+        </div>
+        <Image
+          src={rightPerson}
+          width={123}
+          height={167}
+          alt="rightPerson"
+          className="z-10"
+        />
+        <AuthHeaderVerticalDots styles="right-0 top-0 mt-5 absolute z-0 mr-3" />
+      </div>
+      {children}
+    </>
+  );
+};
+
+export default AuthHeader;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        cerulean: "#0A6AA6",
+        seagull: "#7EB6D9",
+        denim: "#0D5FA6",
+        "cloud-burst": "#354050",
+        midnight: "#29323E",
+      },
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+      },
+      boxShadow: {
+        "3xl": "-1px -1px 24px 3px rgba(0, 0, 0, 0.3)",
+      },
+      backgroundImage: {
+        default: "url('~/public/background.svg')",
+      },
+      screens: {
+        "3xl": "1910px",
+        "4xl": "2010px",
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
