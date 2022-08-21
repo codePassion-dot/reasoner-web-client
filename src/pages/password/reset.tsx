02b505f0f8981168d1c904f241e24bd915559545
Hans Rivera <57734041+hrivera7777@users.noreply.github.com>
2022-08-21 10:46:24 -0500

    feat(auth): created password reset component (#4)
    - created password reset component
    - created pasword reset route
    - added password reset constant
    - added password reset validation and fields

---
+++ src/pages/password/reset.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import AuthPasswordReset from "../../components/AuthPasswordReset";
+import AuthHeaderLayout from "../../layouts/AuthHeaderLayout";
+
+const PasswordReset: NextPage = () => {
+  return (
+    <AuthHeaderLayout title="Password Reset">
+      <AuthPasswordReset />
+    </AuthHeaderLayout>
+  );
+};
+
+export default PasswordReset;
